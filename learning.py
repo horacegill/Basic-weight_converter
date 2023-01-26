072d386d82776b16423a0fc4d023a2bc2d29643d
horatiogillibrand <horatiogillibrand@outlook.com>
2023-01-26 19:42:44 +0000

    remove

--- learning.py
+++
@@ -1,8 +0,0 @@
-weight = int(input("Weight: "))
-unit = input("(K)gs or (L)bs: ")
-if unit.upper() == "K":
-    converted =  weight / 0.45
-    print("Weight in (L)bs "+ str(converted))
-else:
-    converted = weight * 0.45
-    print("Weight in (K)gs "+ str(converted))
